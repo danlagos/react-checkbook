cbe156d7b5ddbfd95f9f066c26d6c0a6effbd749
Daniel Lagos <lagos.d@gmail.com>
2020-02-22 13:12:46 -0800

    hard code transactions component to include pagination.

--- src/components/checkingAccount/transactions.js
+++ src/components/checkingAccount/transactions.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Pagination from '@material-ui/lab/Pagination';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
 function Transactions() {
+  const classes = useStyles();
   return (
     <div className="App">
       <h1> transactions component </h1>
+      <div>
+        <p>Date</p> <p>add date props</p>
+        <p>transaction</p> <p>add pendingTransaction props</p>
+        <p>Amount</p> <p>add amount props</p>
+        <p>Amount</p> <p>add runningTotal output</p>
+      </div>
+      <div className={classes.root}>
+        <Pagination count={10} />
+      </div>
     </div>
   );
 }
