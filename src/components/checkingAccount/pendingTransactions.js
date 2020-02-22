b09be278e9c187c5b97a875744ff25d896e4e5ef
Daniel Lagos <lagos.d@gmail.com>
2020-02-22 13:05:04 -0800

    hard code pending transactions component.

--- src/components/checkingAccount/pendingTransactions.js
+++ src/components/checkingAccount/pendingTransactions.js
@@ -5,6 +5,11 @@ function PendingTransactions() {
   return (
     <div className="App">
       <h1> Pending Transactions Component</h1>
+      <div>
+        <p>Date</p> <p>add date props</p>
+        <p>transaction</p> <p>add pendingTransaction props</p>
+        <p>Amount</p> <p>add amount props</p>
+      </div>
     </div>
   );
 }
