fda027608fd26e2fd1bf592c71356cdf5ded3f92
Daniel Lagos <lagos.d@gmail.com>
2020-02-22 13:00:26 -0800

    hard code transaction serach component.

--- src/components/checkingAccount/transactionSearch.js
+++ src/components/checkingAccount/transactionSearch.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 function TransactionSearch() {
   return (
-    <div className="App">
-      <h1> transaction search component </h1>
+    <div >
+      <h1>Transaction Search</h1>
+      <form>
+        <label>
+          Keywords:
+          <br />
+          <input type="text" name="Keywords" value='Keywords' />
+        </label>
+        <br />
+        <label>
+          Date Range:
+          <br />
+          <input type="text" name="start date" value='start date' />
+          <input type="text" name="end date" value='end date' />
+        </label>
+        <br />
+        <label>
+          Amount Range:
+          <br />
+          <input type="text" name="min" value='min' />
+          <input type="text" name="max" value='max' />
+        </label>
+        <input type="submit" value="Search" />
+      </form>
     </div>
   );
 }
