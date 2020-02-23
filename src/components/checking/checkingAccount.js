74c693d566f85cba1364ba9037720789764e5dae
Daniel Lagos <lagos.d@gmail.com>
2020-02-23 10:40:51 -0800

    create dummy data for transaction app.

--- src/components/checking/checkingAccount.js
+++ src/components/checking/checkingAccount.js
@@ -7,9 +7,15 @@ import Transactions from './transactions.js';
 class CheckingAccount extends Component {
   state = {
     pendingTransData: [
-      { id: 0, date: '1 / 1 / 2020', income: false, transaction: "gas", amount: 25.45 },
-      { id: 1, date: '1 / 2 / 2020', income: false, transaction: "cell phone", amount: 127.35 },
-      { id: 2, date: '1 / 3 / 2020', income: false, transaction: "car payment", amount: 303.97, },
+      { id: 0, date: '1/1/2020', transaction: "gas", amount: -25.45 },
+      { id: 1, date: '1/2/2020', transaction: "cell phone", amount: -127.35 },
+      { id: 2, date: '1/3/2020', transaction: "car payment", amount: -303.97, },
+    ],
+    startBal: 0,
+    transactionData: [
+      { id: 0, date: '1/1/2020', transaction: "gas", amount: -25.45, runningTotal: -25.45 },
+      { id: 1, date: '1/2/2020', transaction: "cell phone", amount: -127.35, runningTotal: -152.8 },
+      { id: 2, date: '1/3/2020', transaction: "car payment", amount: -303.97, runningTotal: -456.77 },
     ]
   }
   render() {
@@ -29,7 +35,6 @@ class CheckingAccount extends Component {
           return <PendingTransactions
             key={pendingTransData.id}
             date={pendingTransData.date}
-            income={pendingTransData.income}
             transaction={pendingTransData.transaction}
             amount={pendingTransData.amount} />
         })}
